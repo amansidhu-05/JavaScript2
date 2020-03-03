96ff0377b3a72ac2efed2b76576bdd6c4935e947
amansidhu-05 <55215908+amansidhu-05@users.noreply.github.com>
2020-03-03 13:19:30 -0500

    Add files via upload
    Bun on the run Menu

---
+++ Assignment3.1.js
@@ -0,0 +1,56 @@
+const menu = [{
+    title: 'Menu 1',
+    soup: 'Minestrone',
+    soupPrice: '3.20',
+    salad: 'Greek',
+    saladPrice: '3.50',
+    lighterFare: 'Vegetable Biryani',
+    lighterFarePrice: '4.00',
+    entree: 'Breaded Chicken on a Bun',
+    entreePrice: '5.99'
+},
+{
+    title: 'Menu 2',
+    soup: 'Red Lentil Dal',
+    soupPrice: '3.50',
+    salad: 'Julienne',
+    saladPrice: '4.00',
+    lighterFare: 'Madras Chicken Salad Wrap',
+    lighterFarePrice: '5.80',
+    entree: 'Fish & Chips',
+    entreePrice: '7.00'
+  },
+  {
+    title: 'Menu 3',
+    soup: 'Spicy Squash & Pumpkin',
+    soupPrice: '2.99',
+    salad: 'Mediterranean',
+    saladPrice: '3.50',
+    lighterFare: 'Ham & Cheese Panini',
+    lighterFarePrice: '5.90',
+    entree: 'Butter Chicken with Rice Pilaf',
+    entreePrice: '6.00'
+  },
+  {
+    title: 'Menu 4',
+    soup: 'Clam Chowder',
+    soupPrice: '3.99',
+    salad: 'Israeli Couscous & Feta',
+    saladPrice: '4.50',
+    lighterFare: 'Pulled Pork on a Bun',
+    lighterFarePrice: '5.30',
+    entree: 'Matar Paneer & Basmati Rice',
+    entreePrice: '7.00'
+  },
+  {
+    title: 'Menu 5',
+    soup: 'Tomato',
+    soupPrice: '3.00',
+    salad: 'Caesar',
+    saladPrice: '4.99',
+    lighterFare: 'Burger & Fries',
+    lighterFarePrice: '5.00',
+    entree: 'Beef & Broccoli with Fried Rice',
+    entreePrice: '6.20'
+  }
+];
